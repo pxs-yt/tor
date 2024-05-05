9ed7225fee5d64a92d8cb3aed0041fc4d4ad211f
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-05-05 16:24:34 +0200

    added ahmia

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("tor-download").onclick = function () {
     location.href = "https://www.torproject.org/dist/torbrowser/13.0.14/tor-browser-windows-x86_64-portable-13.0.14.exe"
 }
+
+document.getElementById("ahmia").onclick = function () {
+    location.href = "juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion"
+}
