9b1fa71a1498589a9108451f86b31d3d9fbc21eb
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-05-05 16:19:23 +0200

    added tor browser download link

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("tor-download").onclick = function () {
+    location.href = "https://www.torproject.org/dist/torbrowser/13.0.14/tor-browser-windows-x86_64-portable-13.0.14.exe"
+}
